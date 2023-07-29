a0513a8f93f11a58b97f7d347226122e32ff9497
artemisfowl <skb655952@gmail.com>
2023-07-29 11:41:22 +0530

    Updated host ip in run

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-from sys import version
-from flask import __version__ as flask_version
-
 from app import application
 
 # fixme: import the application from the init python file under app module
@@ -10,12 +7,7 @@ from app import application
 # fixme: add the code for setting up the right page to the user
 
 def main():
-	print(f"Python version :{version}")
-	print(f"Flask module version :{flask_version}")
-
-	# start the server
-	application.run(debug=True)
-
+	application.run(host="0.0.0.0", debug=True)
 	return 0
 
 if __name__ == "__main__":
